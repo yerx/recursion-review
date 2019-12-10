bf1ac4e7e3a63b4a29201670d85a71aad7fffcc5
yerx <yer.x.xiong@gmail.com>
2019-12-09 19:30:02 -0500

    Working through stringifyJSON.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,38 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+  if(Array.isArray(obj)) {
+    var result = [];
+    for (var i = 0; i < obj.length; i++) {
+      result.push(stringifyJSON(obj[i]));
+    }
+    return '[' + result + ']';
+  }
+
+  if (obj !== null && typeof obj === 'object') {
+    console.log("here");
+   if(Object.keys(obj).length === 0) {
+     return '{}';
+   }
+
+   var result = [];
+
+   var objectKeys = Object.keys(obj);
+
+   for(var k = 0; k < objectKeys.length; k++) {
+     result.push(stringifyJSON(objectKeys[k]) + ':' + stringifyJSON(obj[objectKeys[k]]));
+     console.log(result)
+   }
+
+   return "{" + result + "}";
+  }
+
+
+  if(typeof obj === 'string') {
+    return '"' + obj + '"';
+  } else {
+    return '' + obj;
+  }
+
 };
